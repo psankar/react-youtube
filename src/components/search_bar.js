c36572786707b1fdbc0d6e1faf17ae6fa8ae7fb0
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-11-19 15:45:36 +0530

    16 onChange

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render() {
-        return <input />;
+        return <input onChange={event => console.log(event.target.value)} />;
+    }
+
+    onInputChange(event) {
+        ;
     }
 }
 
