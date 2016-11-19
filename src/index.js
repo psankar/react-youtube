05335e436a5c569df398709dae5c375032bf7a4a
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-11-19 15:28:36 +0530

    14

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY= "AIzaSyBn8TlKFd-NZwADMhhHE6Lrw2zZ0-6cHFc";
 
 const App = () => {
-    return <div>Hello World</div>;
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
