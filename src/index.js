50a72dc8c3d8d09b53e4860984af9621982176e7
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-11-20 16:34:06 +0530

    21 make http request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
-const API_KEY= "AIzaSyBn8TlKFd-NZwADMhhHE6Lrw2zZ0-6cHFc";
+const API_KEY= 'AIzaSyBn8TlKFd-NZwADMhhHE6Lrw2zZ0-6cHFc';
+
+YTSearch({key: API_KEY, term: 'tamil'}, function(data) {
+    console.log(data);
+});
 
 const App = () => {
     return (
