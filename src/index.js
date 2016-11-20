ccd775fd38aa93d62ff2910f3daea27568fb3a1e
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-11-20 22:26:46 +0530

    28 Handling null props

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBn8TlKFd-NZwADMhhHE6Lrw2zZ0-6cHFc';
 
@@ -26,6 +27,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos} />
             </div>
         );
